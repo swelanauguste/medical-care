9f78a6b32753b413fd735946fd9ec6cc54b59f22
swelanauguste <swelanauguste@gmail.com>
2023-03-21 22:21:49 +0000

    entrypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -10,18 +10,18 @@
 
 #     echo "PostgreSQL started"
 # fi
-python manage.py flush --noinput
+# python manage.py flush --noinput
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --username kingship --email kingship.lc@gmail.com --noinput
+# python manage.py createsuperuser --username kingship --email kingship.lc@gmail.com --noinput
 
-python manage.py add_qualifications
-python manage.py add_skills
-python manage.py add_skill_level
-python manage.py add_tags
-python manage.py add_locations
-python manage.py add_work
-python manage.py add_titles
+# python manage.py add_qualifications
+# python manage.py add_skills
+# python manage.py add_skill_level
+# python manage.py add_tags
+# python manage.py add_locations
+# python manage.py add_work
+# python manage.py add_titles
 
 
 python manage.py collectstatic --noinput
