9ad1ee09a70963b32284557a020c0f8440336bb9
swelanauguste <swelanauguste@gmail.com>
2023-03-21 22:09:28 +0000

    entrypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -13,16 +13,16 @@
 
 python manage.py makemigrations
 python manage.py migrate
-# python manage.py flush --noinput
-# python manage.py createsuperuser --username kingship --email kingship.kc@gmail.com --noinput
+python manage.py flush --noinput
+python manage.py createsuperuser --username kingship --email kingship.kc@gmail.com --noinput
 
-# python manage.py add_qualifications
-# python manage.py add_skills
-# python manage.py add_skill_level
-# python manage.py add_tags
-# python manage.py add_locations
-# python manage.py add_work
-# python manage.py add_titles
+python manage.py add_qualifications
+python manage.py add_skills
+python manage.py add_skill_level
+python manage.py add_tags
+python manage.py add_locations
+python manage.py add_work
+python manage.py add_titles
 
 
 python manage.py collectstatic --noinput
